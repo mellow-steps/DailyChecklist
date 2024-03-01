9f4241186b0dacf971f952393f85d6c88df02f52
James Cirillo <jamescirillo@Jamess-MacBook-Air.local>
2024-03-01 13:45:57 +0800

    Initial commit

---
+++ DailyChecklist/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DailyChecklist
+//
+//  Created by James Cirillo on 1/3/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DailyChecklist/DailyChecklistApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyChecklistApp.swift
+//  DailyChecklist
+//
+//  Created by James Cirillo on 1/3/2024.
+//
+
+import SwiftUI
+
+@main
+struct DailyChecklistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
